d87e0a4c95d1a06a73618ca973e510d8d8343008
vodd <vodwow@gmail.com>
2017-03-21 15:56:37 +0100

    update

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,7 @@ router.post('/register', (req, res, next) => {
         email: req.body.email,
         username: req.body.username,
         password: req.body.password
+        password: req.body.password
     });
 
     User.addUser(newUser, (err, user) => {
